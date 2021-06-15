bd36131d68a723a0961e88f796ed546cef664bbf
Bruno Puzoni <bpuzoni@gmail.com>
2021-06-15 19:43:48 -0300

    removendo o knex e o sqlite3

--- src/db/migrations/20210614235713_create_test.js
+++
@@ -1,7 +0,0 @@
-
-exports.up = knex => knex.schema.createTable("teste", table => {
-  table.increments("id");
-  table.text("nome").notNullable();
-});
-
-exports.down = knex => knex.schema.dropTable("teste");
